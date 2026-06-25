be28ef62f09199e7f6854952b3d3ffc7f379912c
ElvisRP22 <eremaycunarp544@gmail.com>
2026-06-25 15:52:03 -0500

    chore(auth): configurar googleClientId para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: '/api',
-  googleClientId: ''
+  googleClientId: '568836453232-rnavnv58b35fp2t7inalkd469afvoomf.apps.googleusercontent.com'
 };
